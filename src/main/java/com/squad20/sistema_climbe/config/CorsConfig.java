14a01c2d5115e9729d865c4a22cdc8987042d31b
RoqueTavares <roque.tavares@souunit.com.br>
2026-05-07 14:04:09 -0300

    update:cors config

--- src/main/java/com/squad20/sistema_climbe/config/CorsConfig.java
+++ src/main/java/com/squad20/sistema_climbe/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOriginsConfig.split(","))
+                .allowedOriginPatterns(allowedOriginsConfig.split(","))
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
